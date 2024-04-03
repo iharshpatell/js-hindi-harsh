8427f93505ae01fd4afc38854cffc6f8e0d5e19e
Harsh Patel <112988055+iharshpatell@users.noreply.github.com>
2024-04-03 20:19:52 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "harsh44@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+accountEmail = "hsc@gmail.com"
+accountPassword = "232434"
+accountCity = "Mumbai"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(5 + 5) // we are using node js not browser
+
+let name = "Harsh"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbole => unique
+
+// object
+
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
