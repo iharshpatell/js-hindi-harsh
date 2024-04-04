14228cadfe59fba0800f5ab16afda4ecd7040980
Harsh Patel <112988055+iharshpatell@users.noreply.github.com>
2024-04-04 08:43:17 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" = 33
+// "33abs" = NaN
+// null = 0
+// undefined = NaN
+// True = 1 ; False = 0 
+
+
+let isLoggedIn = true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
