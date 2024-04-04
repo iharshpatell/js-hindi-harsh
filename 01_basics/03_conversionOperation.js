dc337974cf9f61b9d4231f138c0bb643a390082e
Harsh Patel <112988055+iharshpatell@users.noreply.github.com>
2024-04-04 11:28:58 +0000

    conversion datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,9 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof stringNumber);
+
+//**************************  Operations  ***************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
